bc7c7e4d679640d242aab1cb44564d42dba4dbf7
jayalakshmijilakara <jayalakshmijilakara123@gmail.com>
2024-02-06 12:03:39 +0530

    Custom Changes

---
+++ dell/dell/doctype/custom_dell_doctype/custom_dell_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Jaya and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CustomDellDoctype(Document):
+	pass
---
+++ dell/dell/doctype/custom_dell_doctype/test_custom_dell_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Jaya and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomDellDoctype(FrappeTestCase):
+	pass
